56850c4e68a92b82e0ff0624aef69e33a315303a
Alessandro Varela <alessandrovarela@gmail.com>
2022-11-10 23:23:38 -0500

    add routes and page error

---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+import './logo.css'
+import { Link } from 'react-router-dom'
+
+export function Logo(){
+  return(
+    <Link to={'/'}>
+      <h1 className='logo'>Crivo<span className='logo-text'>Digital</span></h1>
+    </Link>
+  )
+} 
