ab348ebc278b1b725997540805e34b64cff49cd9
Alessandro Varela <alessandrovarela@gmail.com>
2022-11-11 00:19:57 -0500

    add links home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,29 @@
+import './home.css'
+
 export default function Home(){
   return(
-    <div>
-      <h1>Página HOME</h1>
+    <div className='home-container'>
+      <h1>Alessandro Varela</h1>
+      <span>Veja meus links 👇</span>
+      <main className='links'>
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'>Canal no Youtube</p>
+          </a>
+        </section>
+
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'>Grupo Telegram</p>
+          </a>
+        </section>
+
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'>Treinamento</p>
+          </a>
+        </section>
+      </main>
     </div>
   )
 
