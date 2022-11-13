8f57a19f3eb90b0b5ef7d82284f0bae93d084774
Alessandro Varela <alessandrovarela@gmail.com>
2022-11-12 23:22:20 -0500

    add useState login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,32 @@
+import { useState } from 'react'
 import './login.css'
 import {Logo} from '../../components/Logo'
 
 export default function Login(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handLogin(e){
+    e.preventDefault();
+    alert("TESTE")
+  }
+
   return(
     <div className='login-container'>
       <Logo/>
-      <form className='form'>
+      <form className='form' onSubmit={handLogin}>
         <input
           type='email'
-          placeholder='Digite o seu email' 
+          placeholder='Digite o seu email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type='password'
           placeholder='*******'
-          autoComplete='on' 
+          autoComplete='on'
+          value={password}
+          onChange = { (e) => setPassword(e.target.value)}
         />
 
         <button type='submit'>Acessar</button>
